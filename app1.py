6c52d38c6fe1a768caf751763144a9ec59522a33
darshanbachhav0 <darshan.bachhav20@vit.edu>
2025-03-19 17:50:47 +0530

    update

--- app1.py
+++ app1.py
@@ -194,4 +194,4 @@ def speech_to_text_stream():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
