b7c0b0ab1e8ecc4936c8a2f2bf4bcdc26f57f2ad
darshanbachhav0 <153503855+darshanbachhav0@users.noreply.github.com>
2025-09-24 10:16:59 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -16,9 +16,9 @@ from symspellpy import SymSpell, Verbosity
 # import io
 # import torch
 # import numpy as np
-# import nltk
+import nltk
 from nltk.corpus import stopwords
-# import re
+import re
 # from sentence_transformers import SentenceTransformer, util, CrossEncoder
 from rank_bm25 import BM25Okapi
 # from unidecode import unidecode
